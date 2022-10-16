3786d607c71aafaa116ac297fac56103b01bb63a
jiake3109 <jialin.zhang623833@tufts.edu>
2022-10-16 19:50:29 -0400

    1

--- fileclient.cpp
+++ fileclient.cpp
@@ -4,37 +4,9 @@
 //
 //        Author: Ivian Zhang and Jack Zhang
 //
-//
-//        This is a simple client, designed to illustrate use of:
-//
-//            * The C150DgmSocket class, which provides
-//              a convenient wrapper for sending and receiving
-//              UDP packets in a client/server model
-//
-//            * The C150NastyDgmSocket class, which is a variant
-//              of the socket class described above. The nasty version
-//              takes an integer on its constructor, selecting a degree
-//              of nastiness. Any nastiness > 0 tells the system
-//              to occasionally drop, delay, reorder, duplicate or
-//              damage incoming packets. Higher nastiness levels tend
-//              to be more aggressive about causing trouble
-//
-//            * The C150NastyFile class, which provides a convenient
-//              wrapper for causing operations to occasionally misbehave.
-//
-//            * The c150debug interface, which provides a framework for
-//              generating a timestamped log of debugging messages.
-//              Note that the socket classes described above will
-//              write to these same logs, providing information
-//              about things like when UDP packets are sent and received.
-//              See comments section below for more information on
-//              these logging classes and what they can do.
-//
-//
 //        COMMAND LINE
 //
 //              fileclient <server> <networknastiness> <filenastiness> <srcdir>
-//
 //
 //        OPERATION
 //
@@ -59,11 +31,6 @@
 //              for each user, as long as you are registerd in the
 //              the student port mapping table (ask Noah or the TAs if
 //              the program dies because you don't have a port).
-//
-//        LIMITATIONS
-//
-//              This version does not timeout or retry when packets are lost.
-//
 //
 //       Copyright: Ivian Zhang and Jack Zhang
 //
@@ -105,11 +72,12 @@ int main(int argc, char *argv[]) {
     // Grading command 
     // 
     GRADEME(argc, argv);
+    *GRADING << "Start writing to client log.\n";
     //
     // Variable declarations
     //
-    int filenastiness;               // how aggressively do we drop packets, etc?
-    int networknastiness;
+    int filenastiness;
+    // int networknastiness;
 
     //
     // Make sure command line looks right
@@ -129,43 +97,39 @@ int main(int argc, char *argv[]) {
         exit(4);
     }
     filenastiness = atoi(argv[filenastyArg]);
-    networknastiness = atoi(argv[netnastyArg]);
+    // networknastiness = atoi(argv[netnastyArg]);
     string srcdir = argv[srcdirArg];
 
     // end to end check from local to socket
     vector<string> filenames;
     GetFileNames(filenames, srcdir);
     vector<fileProp> allFilesProp;
+    *GRADING << "Starting FileCopyE2ECheck\n";
     FileCopyE2ECheck(filenastiness, srcdir, allFilesProp, filenames);
-    printf("finished with FileCopyE2E\n");
-
-    // end to end check from socket to socket
-    printf("starting FileSendE2ECheck\n");    
-    //      Send / receive / print
-    
-    try {
-
-        // Create the socket
-        C150DgmSocket *sock = new C150NastyDgmSocket(networknastiness);
-
-        // Tell the DGMSocket which server to talk to
-        sock -> setServerName(argv[serverArg]);
-        sock -> turnOnTimeouts(5000);
-
-        // network e2e check
-        FileSendE2ECheck(*sock, allFilesProp, filenames);
-
+    for (long unsigned int index = 0; index < allFilesProp.size(); index++){
+        unsigned char* address;
+        formatRequestBuf(allFilesProp.at(index), &address);
     }
+    *GRADING << "Finished FileCopyE2ECheck\n";
 
-    //
-    //  Handle networking errors -- for now, just print message and give up!
-    //
-    catch (C150NetworkException& e) {
-        // In case we're logging to a file, write to the console too
-        cerr << argv[0] << ": caught C150NetworkException: " << e.formattedExplanation() << endl;
-    }
-    printf("finished with FileSendE2ECheck\n");
+    // // end to end check from socket to socket
+    // try {
+    //     // Create the socket
+    //     C150DgmSocket *sock = new C150NastyDgmSocket(networknastiness);
 
+    //     // Tell the DGMSocket which server to talk to
+    //     sock -> setServerName(argv[serverArg]);
+    //     sock -> turnOnTimeouts(5000);
 
+    //     // network e2e check
+    //     FileSendE2ECheck(*sock, allFilesProp, filenames);
+    // }
+    // //
+    // //  Handle networking errors -- for now, just print message and give up!
+    // //
+    // catch (C150NetworkException& e) {
+    //     // In case we're logging to a file, write to the console too
+    //     cerr << argv[0] << ": caught C150NetworkException: " << e.formattedExplanation() << endl;
+    // }
     return 0;
 }
--- networkendtoend.cpp
+++ networkendtoend.cpp
@@ -15,6 +15,112 @@
 using namespace std;
 using namespace C150NETWORK;  // for all the comp150 utilities
 
+void printSHA1(unsigned char *partialSHA1dup);
+void parseHeaderField(unsigned char *receivedBuf);
+
+/* * * * * *  * * * * * * * * * *  NETWORK CLIENT FUNCTIONS  * * * * * * * * * * * * * * * */
+void 
+FileSendE2ECheck(C150DgmSocket& sock, vector<fileProp>& allFilesProp, vector<string>& filenames)
+{
+    // if one file fails more than 3 times, give up
+    long unsigned int index = 0;
+    unsigned iteration = 0;
+    bool lastfile = false;
+    while (index < allFilesProp.size())
+    {
+        if (index == allFilesProp.size() - 1){lastfile = true;}
+        if (iteration < 10){
+            *GRADING << "File: <" << filenames.at(index) << ">, beginning transmission, attempt <" << iteration << ">\n";
+            if (!sendtoTar(sock, allFilesProp.at(index), iteration, lastfile, filenames.at(index))){continue;}
+        }
+        *GRADING << "File: <" << filenames.at(index) << "> transmission complete, waiting for end-to-end check, attempt <" << iteration << ">\n";
+        index++;
+        iteration = 0;
+    }
+}
+
+/* formatRequestBuf 
+ * purpose: Check if the server sends a confirmation message
+ * parameter: 
+ *            C150DgmSocket& sock: reference to socket object
+ * return: none
+ * notes: N/A
+ */
+void 
+formatRequestBuf(fileProp& singleFile, unsigned char **requestBuf)
+{ 
+    // compute filename's Sha1
+    unsigned char fnsha1[20];
+    ssize_t filenameSize = singleFile.filename.size();
+    SHA1((unsigned char*)singleFile.filename.c_str(), filenameSize, fnsha1);
+    // populate info into stringstream
+    stringstream headerSS;
+    headerSS << singleFile.filename << "\n";
+    headerSS << fnsha1 << "\n";
+    headerSS << singleFile.contentSize << "\n";
+    headerSS << singleFile.fileSHA1 << "\n";
+    string header = headerSS.str();
+    cout << header << "\n\n";
+    const char* h = header.c_str();
+    ssize_t totalBufSize = header.size();
+    char *generateBuf = (char*)malloc((totalBufSize+1) * sizeof(unsigned char));
+    strcpy(generateBuf, h);
+    *requestBuf = (unsigned char*)generateBuf;
+    printSHA1(fnsha1);
+    printSHA1(singleFile.fileSHA1);
+    parseHeaderField(*requestBuf);
+}
+
+void 
+parseHeaderField(unsigned char *receivedBuf){
+    // filename, filenameSHA1, content size, contentSHA1 
+    printf("inside parseHeader Field \n");
+
+    // read filename
+    char filename_str[50];
+    memset(filename_str, '\0', 50);
+    sscanf((const char*)receivedBuf, "%[^\n]", filename_str);
+    cout << filename_str << " is the file name" << endl;
+
+    char *first_new_line = strstr((char *)receivedBuf, "\n");
+    char *second_new_line = strstr((char *)first_new_line, "\n");
+    char *third_new_line = strstr((char *)second_new_line, "\n");
+    char *last_new_line = strstr((char *)third_new_line, "\n"));
+
+    // read filename SHA1
+    char filenameSHA1[20];
+    strncpy(filenameSHA1, (const char*)(first_new_line + 1), 20);
+    unsigned char real_fileSHA1[20];
+    for (int i = 0; i < 20 ; i ++) {
+        real_fileSHA1[i] = (unsigned)filenameSHA1[i];
+    }
+    printSHA1(real_fileSHA1);
+
+    // read content size
+   char contentSize[50];
+   memset(contentSize, '\0', 50);
+   strncpy(contentSize, (const char*)(second_new_line + 1), third_new_line - second_new_line);
+//    printf("file name length is [%ld]\n", strlen(filename_str));
+//    sscanf((const char*)receivedBuf + 22 + strlen(filename_str), "%[^\n]", contentSize);
+   cout << atoi(contentSize) << " is the content size" << endl;
+   printf("content size length is [%ld]\n", strlen(contentSize));
+
+
+//     // read content SHA1 
+//     char fileSHA1[20];
+//     strncpy(fileSHA1, (const char*)(receivedBuf + strlen(filename_str) + 23 + strlen(contentSize)), 20);
+//     // sscanf((const char*)receivedBuf + 20 + strlen(filename_str) + strlen(contentSize),"%[^\n]", fileSHA1);
+//     unsigned char real_contentSHA1[20];
+//     for (int i = 0; i < 20 ; i ++) {
+//         real_contentSHA1[i] = (unsigned)fileSHA1[i];
+//     }
+//     printSHA1(real_contentSHA1);
+
+    
+}
+
+
+
 /* ServerRESCheck 
  * purpose: Check if the server sends a confirmation message
  * parameter: 
@@ -55,20 +161,15 @@ ServerRESCheck(C150DgmSocket& sock, unsigned& iteration)
 bool 
 sendtoTar(C150DgmSocket& sock, fileProp& file, unsigned& iteration, bool& lastfile, string filename)
 {
-    // Send status
-    if (lastfile){
-        sock.write(LAST, strlen(LAST));
-    }else{
-        sock.write(CONT, strlen(CONT));
-    }
-    // check status sent successfully
-    if (!ServerRESCheck(sock, iteration)){return false;}
 
     // Send filename 
     ssize_t filenameSize = strlen(filename.c_str());
     sock.write((const char*)filename.c_str(), filenameSize+1);
     // Check filename sent successfully
-    if (!ServerRESCheck(sock, iteration)){return false;}
+    if (!ServerRESCheck(sock, iteration)){
+        *GRADING << "File: <" << filename << ">, failed at sending filename, attempt <" << iteration + 1 << ">\n";
+        return false;
+    }
     
     // Send filename's Sha1
     unsigned char obuf[20];
@@ -76,8 +177,14 @@ sendtoTar(C150DgmSocket& sock, fileProp& file, unsigned& iteration, bool& lastfi
     sock.write((const char*)obuf, 21);
 
     // Check filename's SHA1 sent successfully
-    if (!ServerRESCheck(sock, iteration)){return false;}
-    if (!ServerRESCheck(sock, iteration)){return false;}
+    if (!ServerRESCheck(sock, iteration)){
+        *GRADING << "File: <" << filename << ">, failed at sending SHA1 for filename, attempt <" << iteration + 1 << ">\n";
+        return false;
+    }
+    if (!ServerRESCheck(sock, iteration)){
+        *GRADING << "File: <" << filename << ">, failed at matching SHA1 for filename, attempt <" << iteration + 1 << ">\n";
+        return false;
+    }
 
     // Send Size 
     string sizeField = to_string(file.contentSize); 
@@ -85,13 +192,19 @@ sendtoTar(C150DgmSocket& sock, fileProp& file, unsigned& iteration, bool& lastfi
     sock.write((const char*)sizeField.c_str(), contentSize);
 
     // Check Size sent successfully
-    if (!ServerRESCheck(sock, iteration)){return false;}
-    
+    if (!ServerRESCheck(sock, iteration)){
+        *GRADING << "File: <" << filename << ">, failed at sending size of file, attempt <" << iteration + 1 << ">\n";
+        return false;
+    }
+
     // send SHA1
     sock.write((const char*)file.fileSHA1, 21);
 
     // check SHA1 sent successfully
-    if (!ServerRESCheck(sock, iteration)){return false;}
+    if (!ServerRESCheck(sock, iteration)){
+        *GRADING << "File: <" << filename << ">, failed at sending SHA1 for file's content, attempt <" << iteration + 1 << ">\n";
+        return false;
+    }
     
     // send Content 
     ssize_t ByteSent = 0;
@@ -101,12 +214,6 @@ sendtoTar(C150DgmSocket& sock, fileProp& file, unsigned& iteration, bool& lastfi
         if (sendtimes == 10){
             unsigned char pobuf[20];
             SHA1((const unsigned char*)(file.contentbuf + ByteSent - CurByteSent), CurByteSent, pobuf);
-            printf("using %ld, sending SHA1[", CurByteSent);
-            for (int i = 0; i < 20; i++)
-            {
-                printf ("%02x", (unsigned int) pobuf[i]);
-            }
-            printf("].\n");
             sock.write((const char*)pobuf, 21);
             if (!ServerRESCheck(sock, iteration)){return false;}
             if (!ServerRESCheck(sock, iteration)){return false;}
@@ -123,16 +230,39 @@ sendtoTar(C150DgmSocket& sock, fileProp& file, unsigned& iteration, bool& lastfi
             ByteSent += WRITESIZE;
         }
         sendtimes++;
-        printf("already sent %ld expecting to send %ld\n", ByteSent, file.contentSize); 
     }
 
     // check Content sent successfully
-    // check size is equal
-    // check sha1 is equal
-    for (int i = 0; i < 3; i++){
-        if (!ServerRESCheck(sock, iteration)){return false;}
+    if (!ServerRESCheck(sock, iteration)){
+        *GRADING << "File: <" << filename << ">, failed at sending file's content, attempt <" << iteration + 1 << ">\n";
+        return false;
     }
+
+    // check size is equal
+    if (!ServerRESCheck(sock, iteration)){
+        *GRADING << "File: <" << filename << ">, failed at matching file's size, attempt <" << iteration + 1 << ">\n";
+        return false;
+    }
+    
+    // check sha1 is equal
+    if (!ServerRESCheck(sock, iteration)){
+        *GRADING << "File: <" << filename << ">, failed at matching SHA1 for file's content, attempt <" << iteration + 1 << ">\n";
+        return false;
+    }
+    
+    *GRADING << "File: <" << filename << ">, transmission complete, waiting for end-to-end check, attempt <" << iteration + 1 << ">\n";
     return true;
+}
+
+/* * * * * *  * * * * * * * * * *  NETWORK SERVER FUNCTIONS  * * * * * * * * * * * * * * * */
+void 
+FileReceiveE2ECheck(C150DgmSocket& sock, vector<fileProp>& allArrivedFiles)
+{
+    *GRADING << "Begining to receive whole files from client." << endl; 
+    while (1){
+        if (readfromSrc(sock, allArrivedFiles)) break;
+    }
+    *GRADING << "Received and successfully read a total of " << allArrivedFiles.size() << " files from client." << endl;
 }
 
 /* readSizefromSocket 
@@ -148,17 +278,14 @@ sendtoTar(C150DgmSocket& sock, fileProp& file, unsigned& iteration, bool& lastfi
 bool 
 readSizefromSocket(C150DgmSocket& sock, size_t bytestoRead, char** bytes_storage)
 {
-    printf("currently reading certain size from socket\n");
     bzero((*bytes_storage), bytestoRead);
     sock.read((*bytes_storage), bytestoRead);
     /* evaluate conditions to send ACK or REJ */
     if (sock.timedout()){ 
-        printf("sock time out \n");
         sock.write(REJ, strlen(REJ));
         return false;
     }
     sock.write(ACK, strlen(ACK));
-    printf("finished sending ACK\n");
     return true;
     
 }
@@ -220,12 +347,10 @@ readContentfromSocket(C150DgmSocket& sock, int contentlen, unsigned char** read_
             }
             if (!compareSHA1(&pobuf, &partialSHA1dup)) {
                 *GRADING << "The received <" << "> bytes SHA1 do not match, requesting resend." << endl;
-                printf("SHA1 not match.\n");
                 sock.write(REJ, strlen(REJ));
                 return false;
             } else {
                 sock.write(ACK, strlen(ACK));
-                printf("finished sending ACK\n");
             }
             readtimes = 0;
             curBytes = 0;
@@ -235,16 +360,13 @@ readContentfromSocket(C150DgmSocket& sock, int contentlen, unsigned char** read_
             free(partialSHA1dup);
         }
         chunk = sock.read((char *)temporaryBuf, BUFSIZE);
-        // printf("reading content complete with %ld bytes.\n", chunk);
         for (ssize_t t = 0; t < chunk; t += sizeof(unsigned char)) {
             /* put read bytes into dynamically expanding memories */
             allFileContent.push_back(*(temporaryBuf + t)); }
-        // printf("populate file content complete.\n");
         bzero(temporaryBuf, chunk); /* clean up the buf for next read */  
         curBytes += chunk;
         totalBytes += chunk;
         readtimes++;
-        // printf("totalbytes currently is %ld expecting %d \n", totalBytes, contentlen); 
         if (sock.timedout()){
             printf("oops timed out.\n");
             sock.write(REJ, strlen(REJ));
@@ -288,13 +410,6 @@ readContentfromSocket(C150DgmSocket& sock, int contentlen, unsigned char** read_
 bool
 readfromSrc(C150DgmSocket& sock, vector<fileProp>& allArrivedFiles)
 {
-    /* 1ACK-receive status message from client */ 
-    char *statusMsg = (char *)malloc(STATUS * sizeof(char));
-    if (!readSizefromSocket(sock, STATUS, &statusMsg)){
-        *GRADING << "Failed to read file transmission status." << endl;
-        return false;
-    } 
-
     /* 2ACK-receive filename from client */
     char *filename = (char*)malloc(BUFSIZE * sizeof(char));
     if (!readSizefromSocket(sock, BUFSIZE, &filename)){
@@ -318,30 +433,30 @@ readfromSrc(C150DgmSocket& sock, vector<fileProp>& allArrivedFiles)
         FileSHA1dup[j] = (unsigned char)filenameSHA1[j];
     }
     if (!compareSHA1(&shabuf, &FileSHA1dup)) {
-        *GRADING << "File: <" << filename_str << "> failed at socket to socket end-to-end check for inconsisntent filename and filename SHA1." << endl;
+        *GRADING << "File: <" << filename_str << "> failed at socket to socket end-to-end check for inconsistency in received and calculated filename SHA1." << endl;
         sock.write(REJ, strlen(REJ));
         return false;
     } else {
         sock.write(ACK, strlen(ACK));
-        *GRADING << "File: <" << filename_str << "> succeed at socket received and calculated filename SHA1." << endl;
+        *GRADING << "File: <" << filename_str << "> succeed at socket to socket end-to-end check for consistency in received and calculated filename SHA1." << endl;
     }
     
     /* 5ACK-receive content size from client */ 
     char *sizeMsg = (char*)malloc(CONTENT_SIZE * sizeof(char));
     if (!readSizefromSocket(sock, CONTENT_SIZE, &sizeMsg)){
-        *GRADING << "File: <" << filename_str << "> socket to socket end-to-end check failed at receiving contents from client." << endl;
+        *GRADING << "File: <" << filename_str << "> failed at socket to socket end-to-end check for failing to receive contents from client." << endl;
         return false;
     }
 
     /* 6ACK-receive content SHA1 from client */
     char *SHA1Msg = (char*)malloc(SHA_MSG * sizeof(char));
     if (!readSizefromSocket(sock, SHA_MSG, &SHA1Msg)){ 
-        *GRADING << "File: <" << filename_str << "> socket to socket end-to-end check failed at receiving SHA1 from client." << endl;
+        *GRADING << "File: <" << filename_str << "> failed at socket to socket check for failing to receive contents SHA1 from client." << endl;
         return false;
     }
     unsigned char *SHA1dup = (unsigned char*)malloc(SHA_MSG * sizeof(char));
     bzero(SHA1dup, 20);
-    for (int j = 0; j < 20; j++){ 
+    for (int j = 0; j < 20; j++){ /* casting received SHA1 */
         SHA1dup[j] = (unsigned char)SHA1Msg[j];
     }
     free(SHA1Msg);
@@ -352,27 +467,25 @@ readfromSrc(C150DgmSocket& sock, vector<fileProp>& allArrivedFiles)
     unsigned char* prod = nullptr;
     if (!readContentfromSocket(sock, contentlen, &prod)){
         /* unsuccessful read of the entire content */
-        *GRADING << "File: <" << filename_str << "> socket to socket end-to-end check failed at receiving content from client." << endl;
+        *GRADING << "File: <" << filename_str << "> failed at socket to socket check for failing to receive contents from client." << endl;
         free(prod);
         return false;
     }
-    *GRADING << "File: <" << filename_str << ">  socket to socket success at receiving the entire content." << endl;
+    *GRADING << "File: <" << filename_str << "> succeed at socket to socket check with receiving the entire content." << endl;
     unsigned char *obuf = (unsigned char*)malloc(SHA_MSG * sizeof(unsigned char));
     SHA1((const unsigned char *)prod, contentlen, obuf);
 
     /*9ACK-check consistency in received and calculated SHA1 to client */
     if (!compareSHA1(&obuf, &SHA1dup)) {
         sock.write(REJ, strlen(REJ));
-        *GRADING << "File: <" << filename_str << "> socket to socket end-to-end check failed at received and calculated content SHA1." << endl;
+        *GRADING << "File: <" << filename_str << "> socket to socket failed at inconsistency in received and calculated content SHA1." << endl;
         return false;
     }
     sock.write(ACK, strlen(ACK)); /* log for purpose of recording failure caused by network nastiness */
-    *GRADING << "File: <" << filename_str << ">  socket to socket end-to-end check success at receiving the correct size and SHA1 of the content." << endl;
-    string status(statusMsg);
+    *GRADING << "File: <" << filename_str << ">  succeed at socket to socket check with consistency in size and SHA1 of the content." << endl;
     fileProp file_unit = fileProp(filename_str, SHA1dup, contentlen, prod);
     allArrivedFiles.push_back(file_unit);
     free(obuf);
-    free(statusMsg);
     free(prod);
     free(SHA1dup);
     free(sizeMsg);
@@ -380,43 +493,15 @@ readfromSrc(C150DgmSocket& sock, vector<fileProp>& allArrivedFiles)
     free(filenameSHA1);
     free(shabuf);
     *GRADING << "File: <" << filename_str << "> socket to socket end-to-end check succeeded." << endl;
-    return (status == LAST) ? true : false;
+    return true;
 }
 
-void 
-FileSendE2ECheck(C150DgmSocket& sock, vector<fileProp>& allFilesProp, vector<string>& filenames)
-{
-    // if one file fails more than 3 times, give up
-    long unsigned int index = 0;
-    unsigned iteration = 0;
-    bool lastfile = false;
-    while (index < allFilesProp.size())
-    {
-        printf("currently sending to socket\n");
-        if (index == allFilesProp.size() - 1){lastfile = true;}
-        if (iteration < 10){
-            *GRADING << "File: <" << filenames.at(index) << ">, beginning transmission, attempt <" << iteration << ">\n";
-            if (!sendtoTar(sock, allFilesProp.at(index), iteration, lastfile, filenames.at(index))){continue;}
-        }
-        *GRADING << "File: <" << filenames.at(index) << "> transmission complete, waiting for end-to-end check, attempt <" << iteration << ">\n";
-        index++;
-        iteration = 0;
-    }
-    printf("sending complete\n");
-}
 
-void 
-FileReceiveE2ECheck(C150DgmSocket& sock, vector<fileProp>& allArrivedFiles)
-{
-    *GRADING << "Begining to receive whole files from client." << endl; 
-    while (1){
-        if (readfromSrc(sock, allArrivedFiles)) break;
-    }
-    *GRADING << "Received and successfully read a total of " << allArrivedFiles.size() << " files from client." << endl;
-}
 
+
+/* Helper function for viewing SHA1 */
 void 
-printSHA1(char *partialSHA1dup)
+printSHA1(unsigned char *partialSHA1dup)
 {
     printf("received SHA1[");
     for (int i = 0; i < 20; i++)
